ffdbd6cf9652ca94a26f534e4708f5b7f9e66b41
IshaDigitalArt <ishadigitalart@gmail.com>
2024-05-09 14:01:49 +0200

    Primer commit

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,70 @@
+import { Component,ViewChild, ElementRef } from '@angular/core';
+import { AuthModel } from '@mean/models';
+import { ApiService, AuthService, ChatService, Message } from '@mean/services';
+import { BaseComponent } from '@mean/shared';
+import { SessionStorageConstants, UriConstants } from '@mean/utils';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent extends BaseComponent<Message> {
+  showTyping = false;
+  messages: Message[] = [];
+  userData: AuthModel.User;
+  inputValue = ''
+  counter = 0;
+
+  @ViewChild('scrolles') scrollesElement!: ElementRef;
+
+  constructor(
+    protected readonly api: ApiService<Message>,
+    private readonly chatService: ChatService,
+    private readonly auth: AuthService
+  ) {
+    super(api);
+    this.userData = this.auth.readFromSesion(SessionStorageConstants.USER_TOKEN).user;
+    this.getMessages();
+    this.chatService.getMessage().subscribe(val => {
+      this.messages = val;
+      setTimeout(() => this.updateScroll(), 0);
+    });
+
+    this.chatService.userListening().subscribe(val => {
+      if (typeof val === 'boolean') {
+        this.showTyping = false;
+      } else {
+        this.showTyping = this.userData.id !== val.id;
+      }
+    })
+  }
+
+  private async getMessages() {
+    this.messages = (await this.searchArrAsync({ url: UriConstants.MESSAGES })).response;
+  }
+  saveMessage() {
+    if (this.inputValue) {
+      const payload = { userId: this.userData.id, content: this.inputValue };
+      this.create({ url: `${UriConstants.MESSAGES}/create`, data: payload })
+      this.inputValue = '';
+      this.stopTyping();
+      this.updateScroll(); // <--- Llamar al método updateScroll()
+    }
+  }
+  deleteMessage(id: number) {
+    this.delete({ url: `${UriConstants.MESSAGES}/${id}` });
+  }
+  startTyping() {
+    this.counter++;
+    if (this.counter === 1) this.chatService.sendTyping(this.userData);
+  }
+  stopTyping() {
+    this.counter = 0;
+    this.chatService.sendTyping(false);
+  }
+  updateScroll() {
+    this.scrollesElement.nativeElement.scrollTop = this.scrollesElement.nativeElement.scrollHeight;
+  }
+
+}
---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,126 @@
+import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApiService } from '@mean/services';
+import { BaseComponent } from '@mean/shared';
+import { UriConstants } from '@mean/utils';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html'
+})
+export class RegisterComponent extends BaseComponent {
+  userImg: File | null = null;
+  errorEmailExists = false;
+  errorMessage?: string;
+  // errorMessage: string =''; es lo mismo
+
+  constructor(
+    protected readonly api: ApiService,
+    protected readonly fb: FormBuilder,
+    protected readonly router: Router
+  ) {
+    super(api);
+    this.formGroup = this.fb.group({
+      firstName: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*'), Validators.minLength(2), Validators.maxLength(50)]],
+      lastName: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*'), Validators.minLength(2), Validators.maxLength(50)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, this.passwordValidator()]],
+      fecha_nacimiento: ['', [Validators.required, this.ageValidator()]],
+      descripcion: ['', Validators.maxLength(250)]
+    });
+  }
+
+  passwordValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const password = control.value;
+      const hasMinLength = password.length >= 6;
+      const hasNumber = /\d/.test(password);
+      const hasSpecialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password);
+
+      if (!hasMinLength) {
+        return { minLength: true };
+      }
+
+      if (!hasNumber) {
+        return { hasNumber: true };
+      }
+
+      if (!hasSpecialChar) {
+        return { hasSpecialChar: true };
+      }
+
+      return null;
+    };
+  }
+
+  ageValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const fechaNacimiento = control.value;
+      const today = new Date();
+      const birthDate = new Date(fechaNacimiento);
+      let age = today.getFullYear() - birthDate.getFullYear();
+      const m = today.getMonth() - birthDate.getMonth();
+      if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+      }
+
+      if (age < 18) {
+        return { underage: true };
+      }
+
+      return null;
+    };
+  }
+
+  onUpload({ files }: { files: FileList }) {
+    this.userImg = files[0];
+  }
+
+  removeFile() {
+    this.userImg = null;
+  }
+
+  public handleregister() {
+    if (this.isFormValid()) {
+      this.loading = true;
+      const formData = new FormData();
+      const { firstName, lastName, email, password, fecha_nacimiento, descripcion } = this.formGroup.value;
+
+      formData.append('firstName', firstName);
+      formData.append('lastName', lastName);
+      formData.append('email', email);
+      formData.append('password', password);
+      formData.append('fecha_nacimiento', fecha_nacimiento);
+      formData.append('descripcion', descripcion);
+      formData.append('active', '1');
+      formData.append('roleId', '2');
+      if (this.userImg) formData.append('img', this.userImg);
+
+      this.createService({ url: `${UriConstants.USERS}/create`, data: formData })
+        .subscribe({
+          next: () => {
+            this.router.navigate(['/login']);
+          },
+          error: error => {
+            if (error.status === 409) {
+              this.errorEmailExists = true;
+              this.errorMessage = error.error.response; // Display the specific error message
+            } else {
+              this.alertConfiguration('ERROR', 'Email ya existente');
+            }
+            this.openAlert();
+            this.loading = false;
+          }
+        });
+    }
+  }
+
+  public override alertConfiguration(severity: 'ERROR' | 'SUCCESS', message: string): void {
+    super.alertConfiguration(severity, message);
+  }
+
+  public override openAlert(): void {
+    super.openAlert();
+  }
+}
